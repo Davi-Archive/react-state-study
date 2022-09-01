e4082aeda1dc576569f0d86b74815f4a98ced319
Davi Oliveira <davi4alves@gmail.com>
2022-08-31 23:04:49 -0300

    Delete src directory

--- src/api/github.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const listRepositories = () => axios
-.get('https://api.github.com/repositories')
-.then(( {data} )=> data)
--- src/components/Item.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Item = ({id, full_name}) => (
-    <li key={id}>{full_name}</li>
-)
-
-export default Item
